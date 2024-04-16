1359bf6d7876f984d005dd8773861905e01a6084
KeenanKistoo <kistook19@gmail.com>
2024-04-16 23:54:54 +0200

    EVERYTHING WORKS!!

--- coding-challenge1/src/components/Card/RecipeCard.jsx
+++ coding-challenge1/src/components/Card/RecipeCard.jsx
@@ -6,21 +6,23 @@ import './RecipeCard.css';
 //console.log(recipes) //Checks whether the recipes are being imported correctly. 
 
 function RecipeCard(props){
-    const {img_url, rec_name, restrictions, cooking_time, ingredients} = props; //Removes repetitive code(i.e. !props.img_url, just img_url).
+    const {img_url, rec_name, restrictions, cooking_time, ingredients, showHide, clickFunc} = props; //Removes repetitive code(i.e. !props.img_url, just img_url).
     
     return (
         <>
-            <article className='pop-up show'>
-                <h1 className='main-heading'>{rec_name}</h1>
-                <img className='rec-image' src={img_url} alt={"An Image of " + rec_name} />
-                <h2 className='sub-head'>{"Diet-Friendly: " + restrictions}</h2>
-                <p className='gen-text'>{cooking_time}</p>
-                <h2 className='sub-head'>{"Core Ingrediants: " + ingredients}</h2>
-                <h3 className='ter-head'>Instructions</h3>
-                <p className='gen-text'>Insert Instructions Here!!!</p>
-                <h3 className='ter-head'>Serving Suggestions:</h3>
-                <p className='gen-text'>Insert Serving Suggestions Here!!!</p>
-                <button className='back-btn'>Back To Recipes</button>
+            <article className={`pop-up ${showHide}`}>
+                <section className='pop-rec'>
+                    <h1 className='main-heading'>{rec_name}</h1>
+                    <img className='rec-image' src={img_url} alt={"An Image of " + rec_name} />
+                    <h2 className='sub-head'>{"Diet-Friendly: " + restrictions}</h2>
+                    <p className='gen-text'>{cooking_time}</p>
+                    <h2 className='sub-head'>{"Core Ingrediants: " + ingredients}</h2>
+                    <h3 className='ter-head'>Instructions</h3>
+                    <p className='gen-text'>Insert Instructions Here!!!</p>
+                    <h3 className='ter-head'>Serving Suggestions:</h3>
+                    <p className='gen-text'>Insert Serving Suggestions Here!!!</p>
+                    <button onClick={clickFunc} className='back-btn'>Back To Recipes</button>
+                </section>
             </article>
         </>
     );
--- coding-challenge1/src/components/List/RecipeList.jsx
+++ coding-challenge1/src/components/List/RecipeList.jsx
@@ -7,23 +7,27 @@ import { Heart } from "@phosphor-icons/react";
 function RecipeList(){
     const [filter, setFilter] = useState(null); //Monitors/Changes the filter buttons
     const [search, setSearch] = useState(""); //Monitors/Changes the searchbox state
-    const [recipes, setRecipes] = useState(recipesData); /*Used state change to store the recipe data so that it can be altered
-                                                            without changing the orignal data set*/
+    const [recipes, setRecipes] = useState(recipesData);/*Used state change to store the recipe data so that it can be altered
+    without changing the orignal data set*/
+    const [showFavorites, setShowFavorites] = useState(false);//Manages the state of whether only favourites are showing or if all recipes are showing.
+    const [classFavorite, setClassFavorite] = useState(false); //Controls the class to show when favourites are only being shown.
+    const favMessage = "If you're not seeing any recipes, it means that you haven't favorited any yet. Click on the 'Favorites' button again to view all recipes. You might find some favorites there that you haven't marked yet. Click on the <3 to mark them!"
+    //The above message comes up whenever you are viewing your favorite recipes.
+    const [id, setId] = useState(0); //Controls the data of the recipe card that the user would like to view. Check lines 68-74
+    const [showCard, setShowCard] = useState("hide")
     
-
-    function handleFilter(restriction){
-        setFilter(restriction);
-        
-    }
-    /*This function is used to test buttons when debugging code.
+    /*TestFunction is used to test buttons when debugging code.
     When a button is not acting as expected, my first debug test
     is to check if the button is actually working or not.
     */
-    function TestFunction(){
-        console.log("Button Active")
+    /* function TestFunction(){
+         console.log("Button Active")
+     }*/
+
+    function handleFilter(restriction){
+        setFilter(restriction);
     }
 
-    //Updates search state with value in input field (className: search-box)
     function handleSearch(e){
         setSearch(e.target.value);
     }
@@ -34,46 +38,58 @@ function RecipeList(){
                 recipe.id === id ? { ...recipe, favorite: !recipe.favorite } : recipe
             )
         );
-        console.log(recipes);
     }
-    /*This conntrols what recipes are being displayed. There are two main checks:
-    1) if the filter button is clicked, it filters those recipes
-    2) if there is a value in your search box.
-    */
 
-    function handleFavorites(){
-        setFilter("Favorites")
-    }
-    const filteredRecipes = recipes.filter(recipe => 
-        (!filter || recipe.restrictions === filter) &&
-        (recipe.rec_name.toLowerCase().includes(search.toLowerCase()))
-    );
-
-    const favoriteRecipes =recipes.filter(recipe => 
-        (recipe.favorite === true) &&
-        (recipe.rec_name.toLowerCase().includes(search.toLowerCase()))
-    );
-
-    const [displayRecipes, setDisplayRecipes] = useState(filteredRecipes);
-
+    
+    //Swaps between showing all recipes or just favourites
     function swapRecipes(){
-        if(displayRecipes === filteredRecipes ){
-            setDisplayRecipes(favoriteRecipes)
-        }else if(displayRecipes === favoriteRecipes){
-            setDisplayRecipes(filteredRecipes)
-        }
+        setShowFavorites(!showFavorites);
+        setClassFavorite(!classFavorite);
     }
-    console.log(displayRecipes);
-    //if statement to select which recipes to display
+
+    // Apply filters based on current display mode (all or favorite)
+    const filteredRecipes = recipes.filter(recipe => 
+        (!filter || (filter === "Favorites" ? recipe.favorite : recipe.restrictions === filter)) &&
+        (recipe.rec_name.toLowerCase().includes(search.toLowerCase()))
+    );
+
+    // Update display recipes based on current mode
+    const displayRecipes = showFavorites ? filteredRecipes.filter(recipe => recipe.favorite) : filteredRecipes;
+
+    function SelectRecipe(id){
+        setId(id);
+        setShowCard("show")
+    }
+
+    function toggleShowCard(){
+        setShowCard("hide")
+    }
 
     return (  
         <>
             <h1 className="main-head">Interactive Recipe App</h1>
             <section className="filter">
-                <button 
+            <section>
+                { <RecipeCard
+                clickFunc={() => toggleShowCard()}
+                showHide={showCard}
+                rec_name={recipesData[id].rec_name}
+                img_url={recipesData[id].img_url}
+                restrictions={recipesData[id].restrictions}
+                cooking_time={recipesData[id].cooking_time}
+                ingredients={recipesData[id].ingredients}
+                /> }
+            </section>
+            <button 
                     className={filter === null ? "filter-btns active" : "filter-btns"}
                     onClick={() => handleFilter(null)}>
                         Show All
+                </button>
+                <button 
+                    className={classFavorite ? "filter-btns active" : "filter-btns"}
+                    onClick={swapRecipes}
+                >
+                    Favorites
                 </button>
                 <button 
                     className={filter === "Keto" ? "filter-btns active" : "filter-btns"}
@@ -105,11 +121,7 @@ function RecipeList(){
                     onClick={() => handleFilter("Vegan")}>
                         Vegan
                 </button>
-                <button 
-                    className={filter === "Favorites" ? "filter-btns active" : "filter-btns"}
-                    onClick={() => swapRecipes()}>
-                        Favourite
-                </button>
+                {/* Search input */}
                 <section className="search-sect">
                     <input className="search-box"
                      type="text"
@@ -117,26 +129,29 @@ function RecipeList(){
                      value={search}
                      onChange={handleSearch} />
                 </section>
+                <section>
+                    <p  id="fav-message">{showFavorites ? favMessage : ""}</p>
+                </section>
             </section>
-                {displayRecipes.map((recipe) => (
-                    <article className="recipe-item" key={recipe.id}>
-                        <img className="rec-img" src={recipe.img_url} alt={"Image of " + recipe.rec_name}/>
-                        <section className="info">
-                            <h2 onClick={TestFunction} className="rec-head">{recipe.rec_name}</h2>
-                            <p className="gen-txt">Restrictions: {recipe.restrictions}</p>
-                            <p className="gen-txt">Cooking Time: {recipe.cooking_time}</p>
-                            <button 
-                                onClick={() => toggleFavorite(recipe.id)}
-                                className={recipe.favorite ? 'fav-btn selected' : 'fav-btn'}
-                            >
-                                <Heart size={24}/>
-                            </button>
-
-                        </section>
-                    </article>
-                ))}
+            {/* Display filtered recipes */}
+            {displayRecipes.map((recipe) => (
+                <article className="recipe-item" key={recipe.id}>
+                    <img className="rec-img" src={recipe.img_url} alt={"Image of " + recipe.rec_name}/>
+                    <section className="info">
+                        <h2 onClick={() => SelectRecipe(recipe.id)} className="rec-head">{recipe.rec_name}</h2>
+                        <p className="gen-txt">Restrictions: {recipe.restrictions}</p>
+                        <p className="gen-txt">Cooking Time: {recipe.cooking_time}</p>
+                        <button 
+                            onClick={() => toggleFavorite(recipe.id)}
+                            className={recipe.favorite ? 'fav-btn selected' : 'fav-btn'}
+                        >
+                            <Heart size={24}/>
+                        </button>
+                    </section>
+                </article>
+            ))}
         </>
     );
 }
- 
+
 export default RecipeList;
